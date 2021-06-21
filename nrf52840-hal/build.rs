3695792f90ffb552bafdde39ba68fc0c2931f8c0
Kaspar Schleiser <kaspar@schleiser.de>
2021-06-21 12:05:27 +0200

    nrf52840-hal: use ld-memory for creating memory.x

--- nrf52840-hal/build.rs
+++ nrf52840-hal/build.rs
@@ -1,17 +1,25 @@
 use std::env;
-use std::fs::File;
-use std::io::Write;
 use std::path::PathBuf;
 
+use ld_memory::{Memory, MemorySection};
+
 fn main() {
+    // generate linker script
+    let memory = Memory::new()
+        .add_section(MemorySection::new("RAM", 0x20000000, 256 * 1024))
+        .add_section(
+            MemorySection::new("FLASH", 0x0, 1024 * 1024)
+                .pagesize(4096)
+                .from_env_with_prefix("NRF52840_FLASH"),
+        );
+
     // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    File::create(out.join("memory.x"))
-        .unwrap()
-        .write_all(include_bytes!("memory.x"))
-        .unwrap();
+    memory
+        .to_file(out.join("memory.x"))
+        .expect("writing memory.x");
+
     println!("cargo:rustc-link-search={}", out.display());
 
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=memory.x");
 }
